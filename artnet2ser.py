1c167341b3bad3dd6ff6c5faeb2d9b1ebdd4d31b
David Sawatzke <d-git@sawatzke.dev>
2021-06-28 23:36:25 +0200

    Move artnet packet parsing from host to fw

--- artnet2ser.py
+++ artnet2ser.py
@@ -6,16 +6,11 @@ port = 6454
 
 udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 udp_socket.bind((ip, port))
-
-print("hidden_nonechowrite")
+f = open("artnetdata", "wb")
+f.write(b"\nhidden_nonechowrite\n")
 while True:
     data, adr = udp_socket.recvfrom(1024 * 1024)
     if data[0:8] == b"Art-Net\0":
-        length = (data[16] << 8) | data[17]
-        universe = data[14] + (data[15] << 8)
-        for i in range(length//3):
-            adr = 18 + i * 3
-            rgb = data[adr] + (int(data[adr + 1]) << 8) + (data[adr + 2] << 16)
-            print("write " + hex(0x40200000 + (universe * 170 + i) * 4) + " " + hex(rgb))
-
-print("Bye")
+        f.write(b"nb")
+        f.write(bytearray([len(data) & 0xFF, (len(data) >> 8) & 0xFF]))
+        f.write(data)
--- sw/artdmx.c
+++ sw/artdmx.c
@@ -1,76 +1,62 @@
 #include "artdmx.h"
 #include <console.h>
+#include <generated/mem.h>
 #include <irq.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <system.h>
 #include <uart.h>
 
-static char *readstr(void) {
-  char c[2];
-  static char s[64];
-  static int ptr = 0;
+#define ARTNET_LENGTH (18 + 3 * 170)
 
-  if (readchar_nonblock()) {
-    c[0] = readchar();
-    c[1] = 0;
-    switch (c[0]) {
-    case 0x07:
-      break;
-    case '\r':
-    case '\n':
-      s[ptr] = 0x00;
-      ptr = 0;
-      return s;
-    default:
-      if (ptr >= (sizeof(s) - 1))
-        break;
-      s[ptr] = c[0];
-      ptr++;
-      break;
-    }
+static int processartnet(uint8_t buffer[ARTNET_LENGTH]) {
+  if (strncmp((char *)buffer, "Art-Net", 8) != 0) {
+    puts("Invalid header");
+    return -1;
   }
-
-  return NULL;
-}
-
-static char *get_token(char **str) {
-  char *c, *d;
-
-  c = (char *)strchr(*str, ' ');
-  if (c == NULL) {
-    d = *str;
-    *str = *str + strlen(*str);
-    return d;
+  if ((buffer[8] != 0) | (buffer[9] != 0x50)) {
+    puts("Invalid command");
+    return -1;
   }
-  *c = 0;
-  d = *str;
-  *str = c + 1;
-  return d;
+  if ((buffer[10] != 0) | (buffer[11] != 14)) {
+    puts("Invalid version");
+    return -1;
+  }
+  uint16_t universe = buffer[14] | (buffer[15] << 8);
+  uint16_t length = buffer[17] | (buffer[16] << 8);
+  if (length > 510) {
+    printf("Invalid length of  %x", length);
+    return -1;
+  }
+  volatile uint32_t *sdram_img_base =
+      (volatile uint32_t *)(MAIN_RAM_BASE + (MAIN_RAM_SIZE / 2));
+  for (uint16_t i = 0; i < (length / 3); i++) {
+    uint32_t adr = 18 + i * 3;
+    uint32_t rgb =
+        buffer[adr] + (buffer[adr + 1] << 8) + (buffer[adr + 2] << 16);
+    *(sdram_img_base + universe * 170 + i) = rgb;
+  }
+  return 0;
 }
 
 void nonechowrite(void) {
+  static uint8_t buffer[ARTNET_LENGTH];
   while (1) {
-    char *str;
-    char *token;
-
-    str = readstr();
-    while (str == NULL) {
-      str = readstr();
+    if (readchar() != 'n' || readchar() != 'b') {
+      puts("Invalid udp header");
+      return;
     }
-    token = get_token(&str);
-    if (strcmp(token, "write") == 0) {
-      char *endptr;
-      uint32_t adr = strtol(get_token(&str), &endptr, 16);
-      uint32_t dat = strtol(get_token(&str), &endptr, 16);
-      volatile uint32_t *ptr = (volatile uint32_t *)adr;
-      *ptr = dat;
-      flush_l2_cache();
-    } else {
-      puts("String:");
-      puts(str);
-      puts("done");
+    uint16_t len = (uint16_t)readchar() | ((uint16_t)readchar() << 8);
+    if (len > ARTNET_LENGTH) {
+      printf("Invalid udp length of %x", len);
+      return;
+    }
+    for (uint16_t i = 0; i < len; i++) {
+      buffer[i] = (uint8_t)readchar();
+    }
+    if (processartnet(buffer) != 0) {
       return;
     }
   }

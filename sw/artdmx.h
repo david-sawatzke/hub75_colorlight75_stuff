95f88e8def3ed5a799eb9985c68b0e718a4c769e
David Sawatzke <d-git@sawatzke.dev>
2021-09-22 20:27:03 +0200

    Delete old C sw
    Outdated and probably not that useful anymore

--- sw/artdmx.h
+++
@@ -1,6 +0,0 @@
-#ifndef ARTDMX_H_
-#define ARTDMX_H_
-
-void nonechowrite(void);
-
-#endif // ARTDMX_H_
--- sw/img_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef IMG_UTILS_H_
-#define IMG_UTILS_H_
-
-void init_img_indexed_from_header(void);
-void init_img_from_header(void);
-void init_img_from_spi(void);
-
-#endif // IMG_UTILS_H_

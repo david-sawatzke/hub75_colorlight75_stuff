869a8f82194b0aa2ae7315e2ca239905749dc34a
David Sawatzke <d-git@sawatzke.dev>
2021-12-03 14:03:34 +0100

    Add hacky test packet code

---
+++ artnetsender.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# Hacky test script to generate individual packets
+
+import socket
+
+
+def prepare_artnet_packet():
+    universe = 0
+    color_data = [[0, 0, 0] for _ in range(5)]
+
+    color_data[0] = [255, 255, 255]
+    color_data[1] = [127, 0, 0]
+    # color_data[1] = [0, 0, 0]
+    # color_data[2] = [0, 0, 0]
+    # color_data[3] = [0, 0, 0]
+    # color_data[4] = [0, 0, 0]
+    # color_data[5] = [0, 0, 0]
+    length = len(color_data)
+
+    header = bytearray(b"Art-Net\0\0\x50\x00\x0e\x00\00")
+    # print(len(header))
+
+    header.append(universe & 0xFF)
+    header.append((universe >> 8) & 0xFF)
+    header.append((length >> 8) & 0xFF)
+    header.append(length & 0xFF)
+    data = bytearray()
+    for color in color_data:
+        for val in color:
+            data.append(val)
+    return header + data
+
+
+def send_udp_packet(data):
+    destination = "192.168.1.51"
+    # destination = "127.0.0.1"
+    port = 6454
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+    sock.sendto(data, (destination, port))
+
+
+data = prepare_artnet_packet()
+send_udp_packet(data)
